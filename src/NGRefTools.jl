b35f87e8b4fb939743f2159ef96ef3bdf2f9afb4
Michael Mattocks <mike.mattocks@utoronto.ca>
2020-11-12 02:50:01 -0500

    print tools

--- src/NGRefTools.jl
+++ src/NGRefTools.jl
@@ -1,9 +1,9 @@
 module NGRefTools
-    using ConjugatePriors, Distributions
+    using ConjugatePriors, Distributions, Plots
 
     include("NGRef.jl")
     export marginals
     include("NIGRef.jl")
     include("MarginalTDist.jl")
-    export MarginalTDist, MTDist_MC_func
+    export MarginalTDist, MTDist_MC_func, plot_n_MTDist, plot_logn_MTDist, mean_mass_comparator, log_mean_mass_comparator
 end # module
